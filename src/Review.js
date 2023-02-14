89a800f70a73320ce65d8f7be95b0acb839af870
Aaron <aaron1venema@gmail.com>
2023-02-14 13:00:09 -0600

    project 3

---
+++ src/Review.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const {name,job,image,text} = people[index];
+
+  const nextPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index + 1;
+      if(newIndex >= people.length){
+        newIndex = 0;
+      }
+      return newIndex
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index - 1;
+      if(newIndex < 0){
+        newIndex = people.length-1;
+      }
+      return newIndex
+    })
+  }
+
+  const surpriseMe = () => {
+    const randomNumber = Math.floor(Math.random()*(people.length))
+    setIndex(randomNumber);
+  }
+
+  return (
+    <article className='review'>
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className='prev-btn'>
+          <FaChevronLeft onClick={prevPerson}/>
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight onClick={nextPerson}/>
+        </button> 
+      </div>
+        <button onClick={surpriseMe} className='random-btn'>
+          suprise me
+        </button>
+    </article>
+  );
+};
+
+export default Review;
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Gabe Stephanovich',
+    job: 'Senior Site Reliability Engineer',
+    image:
+      '/gabe.jpg',
+    text:
+      "Aaron has always been an extremely hard worker and learns at an astonishing rate. He would be a great asset to any employer.",
+  },
+  {
+    id: 2,
+    name: 'Wayne Cao',
+    job: 'Owner & Founder',
+    image:
+      '/wayne.jpg',
+    text:
+      "Aaron was punctual and professional when he worked for our school. He was highly spoken by his students' parents and he will be a great asset to your company.",
+  },
+  {
+    id: 3,
+    name: 'rose bays',
+    job: 'Head of human resources',
+    image:
+      '/rose.jpg',
+    text:
+      'Aaron always was dependable, hard working and dedicated to his work. His passion for helping others, striving for excellence and working hard were some of his best qualities. He will be an asset to any organization!',
+  },
+  {
+    id: 4,
+    name: 'Kelly Rock',
+    job: 'Senior Software Architect',
+    image:
+      '/kelly.jpg',
+    text:
+      'Aaron has demonstrated exceptional technical skills in web development, particularly in HTML, CSS, JavaScript and React, and has consistently produced high-quality work that meets and exceeds expectations. In addition to Aaron’s technical abilities, he is a quick learner and a dedicated individual.',
+  },
+  {
+    id: 5,
+    name: 'Florian Segelken',
+    job: 'Technical director',
+    image:
+      '/florian.jpg',
+    text:
+      'I rarely met somebody with such a brilliant mind and dedication - whatever Aaron puts his focus on he will drive to perfection, no matter if in athletics or business - you are truely an inspiring human being and reliable friend!',
+  },
+  {
+    id: 6,
+    name: 'alex hessler',
+    job: 'Junior web developer',
+    image:
+      '/alex.jpg',
+    text:
+      "I was consistently impressed by Aaron's technical skills and ability to learn new technologies quickly. He has a strong foundation in a variety of programming languages, including JavaScript, Python, and HTML/CSS, and is always eager to expand his skillset.",
+  },
+];
+
+export default reviews;
